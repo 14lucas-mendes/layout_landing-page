5723ff1768cd12bdec36e36b451b12d78bc7ca84
Lucas Mendes <164028753+14lucas-mendes@users.noreply.github.com>
2025-07-16 18:00:54 -0300

    make build

---
+++ layout_landing-page.3c72bfe4.js
@@ -0,0 +1,2 @@
+"use strict";document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".footer__form");e&&e.addEventListener("submit",function(t){t.preventDefault(),e.reset()})});
+//# sourceMappingURL=layout_landing-page.3c72bfe4.js.map
